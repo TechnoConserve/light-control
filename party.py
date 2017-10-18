5dc088bce993690f3a74182c2eb8af2efcceff4e
Avery Uslaner <uslaner.avery@gmail.com>
2017-10-18 16:13:39 -0600

    Initial commit.

---
+++ party.py
@@ -0,0 +1,42 @@
+from time import sleep
+
+from bibliopixel.led import *
+from bibliopixel.animation import BaseStripAnim, OffAnim
+from bibliopixel.drivers.LPD8806 import *
+
+class StripTest(BaseStripAnim):
+    def __init__(self, led, start=0, end=-1):
+        #The base class MUST be initialized by calling super like this
+        super(StripTest, self).__init__(led, start, end)
+        #Create a color array to use in the animation
+        self._colors = [colors.Red, colors.Orange, colors.Yellow, colors.Green, colors.Blue, colors.Indigo]
+
+    def step(self, amt = 1):
+        #Fill the strip, with each sucessive color
+        for i in range(self._led.numLEDs):
+            self._led.set(i, self._colors[(self._step + i) % len(self._colors)])
+        #Increment the internal step by the given amount
+        self._step += amt
+        sleep(0.1)
+
+
+def init():
+	#create driver for a 162 pixels
+        driver = DriverLPD8806(162, c_order=ChannelOrder.GRB, use_py_spi=True, dev='/dev/spidev0.0', SPISpeed=16)
+        led = LEDStrip(driver)
+        return led
+
+
+def start_party():
+	led = init()
+	anim = StripTest(led)
+	anim.run(fps=8, seconds=60)
+
+
+def stop_party():
+	led = init()
+	anim = OffAnim(led)
+	anim.run()
+	
+if __name__ == '__main__':
+	start_party()
---
+++ whoshome.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python
+
+import bluetooth
+import time
+import os
+import datetime
+
+import party
+
+start = datetime.time(7, 00)
+end = datetime.time(21, 30)
+partytime = False
+
+while True:
+    now = datetime.datetime.now().time()
+    print("The time is: " + str(now))
+
+    if start <= now <= end or partytime:
+        print("Checking if my master is within range...")
+        try:
+            result = bluetooth.lookup_name('24:4B:81:1F:33:E4', timeout=5)
+            print("Result: ", result)
+
+            if result is not None or partytime:
+                print ("...he's here...Muahahahahaha")
+                party.start_party()
+            else:
+                print("No one home. A pity.")
+                party.stop_party()
+                time.sleep(10)
+
+        except bluetooth.btcommon.BluetoothError as error:
+            print("Could not connect: {}. Retrying in 30 seconds.".format(error))
+            time.sleep(30)
+
+    else:
+        party.stop_party()
+        time.sleep(10)
