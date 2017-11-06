5540d7df271f3069802b04ba21cdd30103854e3d
Avery Uslaner <uslaner.avery@gmail.com>
2017-11-06 16:06:57 -0700

    Gracefully handle killing the the script.

--- whoshome.py
+++ whoshome.py
@@ -3,6 +3,7 @@
 import bluetooth
 import time
 import datetime
+import signal
 
 import party
 
@@ -10,28 +11,47 @@ start = datetime.time(7, 00)
 end = datetime.time(21, 30)
 partytime = False
 
-while True:
-    now = datetime.datetime.now().time()
-    print("The time is: " + str(now))
 
-    if start <= now <= end or partytime:
-        print("Checking if my master is within range...")
-        try:
-            result = bluetooth.lookup_name('24:4B:81:1F:33:E4', timeout=5)
-            print("Result: ", result)
+class GracefulKiller():
+    kill_now = False
 
-            if result is not None or partytime:
-                print ("...he's here...Muahahahahaha")
-                party.start_party()
-            else:
-                print("No one home. A pity.")
-                party.stop_party()
-                time.sleep(10)
+    def __init__(self):
+        signal.signal(signal.SIGINT, self.exit_gracefully)
+        signal.signal(signal.SIGTERM, self.exit_gracefully)
 
-        except bluetooth.btcommon.BluetoothError as error:
-            print("Could not connect: {}. Retrying in 30 seconds.".format(error))
-            time.sleep(30)
+    def exit_gracefully(self, signum, frame):
+        self.kill_now = True
 
-    else:
-        party.stop_party()
-        time.sleep(10)
+
+if __name__ == '__main__':
+    killer = GracefulKiller()
+    while True:
+        now = datetime.datetime.now().time()
+        print("The time is: " + str(now))
+
+        if start <= now <= end or partytime:
+            if killer.kill_now:
+                break
+            print("Checking if my master is within range...")
+            try:
+                result = bluetooth.lookup_name('24:4B:81:1F:33:E4', timeout=5)
+                print("Result: ", result)
+
+                if result is not None or partytime:
+                    print ("...he's here...Muahahahahaha")
+                    party.start_party()
+                else:
+                    print("No one home. A pity.")
+                    party.stop_party()
+                    time.sleep(10)
+
+            except bluetooth.btcommon.BluetoothError as error:
+                print("Could not connect: {}. Retrying in 30 seconds.".format(error))
+                time.sleep(30)
+
+        else:
+            party.stop_party()
+            time.sleep(10)
+
+    party.stop_party()
+    print("Script killed gracefully.")
